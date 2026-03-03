97d813ade4a330d7c3c9735d31c9dbe18b61d9dd
faby <fabianyevellyn9@gmail.com>
2026-03-03 08:38:50 -0300

    Add files via upload

---
+++ soma.py
@@ -0,0 +1,8 @@
+a = float(input("Digite um número: "))
+b = float(input("Digite outro número: "))
+
+soma = a + b
+produto = a * b 
+
+print("A soma dos números é de:", soma)
+print("O produto dos produtos é de:", produto)
